5dde71e3077da6e2980358deb12570462abe0bc1
DivakarRaju <divakarrj875@gmail.com>
2021-06-13 14:57:44 +0530

    added s3 and pull to lambda

--- terraform_infra/providers.tf
+++ terraform_infra/providers.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-    access_key                  = "mock_access_key"
-  region  = var.aws_region
-#   profile = var.profile
-    secret_key                  = "mock_secret_key"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
+#   alias      = "local"
+  access_key = "mock_access_key"
+  region     = var.aws_region
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
-    endpoints {
-      s3             = "http://localhost:4566"
-      lambda         = "http://localhost:4566"
-      iam            = "http://localhost:4566"
-    }
+  endpoints {
+    s3     = "http://localhost:4566"
+    lambda = "http://localhost:4566"
+    iam    = "http://localhost:4566"
+  }
 }
 
 provider "archive" {}
--- terraform_infra/resource.tf
+++ terraform_infra/resource.tf
@@ -1,9 +1,21 @@
+resource "aws_s3_bucket" "zip_file_bucket" {
+  bucket = "sample-filebucket-terraform"
+  #   acl    = "public-read-write"
+#   provider = aws.local
+}
 
 data "archive_file" "ping_pong_file_archive" {
   type        = "zip"
   source_file = "../function/sample.py"
   output_path = "../function/sample.zip"
 }
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.zip_file_bucket.id
+  key    = "sample.zip"
+  source = data.archive_file.ping_pong_file_archive.output_path
+}
+
 resource "aws_iam_role" "iam_for_lambda_tf" {
   name = "iam_for_lambda_tf"
 
@@ -25,11 +37,19 @@ EOF
 }
 
 resource "aws_lambda_function" "ping_pong" {
-  filename      = "../function/sample.zip"
+  #   filename      = data.archive_file.ping_pong_file_archive.output_path
+  # filename = aws_s3_bucket_object.object.source
+  s3_bucket     = aws_s3_bucket.zip_file_bucket.id
+  s3_key        = aws_s3_bucket_object.object.key
   function_name = "ping_pong"
   role          = aws_iam_role.iam_for_lambda_tf.arn
   handler       = "sample.ping_pong"
 
   runtime = "python3.8"
+
+  depends_on = [
+    data.archive_file.ping_pong_file_archive,
+    aws_s3_bucket_object.object
+  ]
 
 }
